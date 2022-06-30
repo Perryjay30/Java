de55436825102e37e46b3700cac6a3adfd9399fe
Perryjay30 <pelumijsh@gmail.com>
2022-06-30 09:32:41 -1200

    AutomaticBike

---
+++ src/PetrolStation/UnicornOil.java
@@ -0,0 +1,22 @@
+package PetrolStation;
+
+public class UnicornOil {
+    private int litrePurchased;
+    private final double pricePerLitre = 200.00;
+    private double percentageDiscount;
+    private  double totalCharges;
+
+    public void setQuantityPurchased(int litrePurchased){
+        this.litrePurchased = litrePurchased;
+    }
+    public double getPercentageDiscount(){
+        double percentageDiscount = (pricePerLitre * litrePurchased) * (0.02);
+        this.percentageDiscount = percentageDiscount;
+        return percentageDiscount;
+    }
+
+    public double getTotalCharges(){
+        totalCharges = (pricePerLitre * litrePurchased) - percentageDiscount;
+        return totalCharges;
+    }
+}
