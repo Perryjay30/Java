fe0302bf4764970cab0c75ccbff4f2ca975b0900
Perryjay30 <pelumijsh@gmail.com>
2022-06-30 15:28:47 -1200

    TriangleDisplay

--- src/numberline/NumberLine.java
+++ src/numberline/NumberLine.java
@@ -1,16 +1,1 @@
-//package numberline;
-//
-//import java.util.Scanner;
-//
-//public class Rowrun {
-//
-//    public static void main(String[] args){
-//        int row = 5;
-//                int firstnum; int secondnum;
-//                for( firstnum < secondnum; firstnum ++);
-//
-//
-//
-//        }
-//    }
 
---
+++ src/tddClass3/TriangleDisplay.java
@@ -0,0 +1,14 @@
+package tddClass3;
+
+public class TriangleDisplay {
+    public static void main(String[] args){
+        System.out.println("1");
+
+        System.out.println("1 2");
+
+        System.out.println("1 2 3");
+
+        System.out.println("1 2 3 4");
+
+    }
+}
