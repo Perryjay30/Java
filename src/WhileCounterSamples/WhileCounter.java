500c605ea654bab960b9e11dc84339f12c0b9b13
Perryjay30 <pelumijsh@gmail.com>
2022-07-07 17:27:24 -1200

    WhileCounter

---
+++ src/WhileCounterSamples/WhileCounter.java
@@ -0,0 +1,15 @@
+package WhileCounterSamples;
+
+public class WhileCounter {
+
+    public static void main(String[] args){
+        int counter = 0;
+
+        while(counter < 10){
+            counter++;
+            System.out.printf("%d%n", counter);
+        }
+
+        System.out.println();
+    }
+}
