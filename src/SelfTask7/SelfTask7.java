cf7c2840bf372ac3f6d0f8714ef48fdf6b5a9ab5
Perryjay30 <pelumijsh@gmail.com>
2022-07-09 23:45:37 -1200

    self Tasks

---
+++ src/SelfTask7/SelfTask7.java
@@ -0,0 +1,18 @@
+package SelfTask7;
+
+import java.util.Scanner;
+
+public class SelfTask7 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter an integer");
+        int casey = input.nextInt();
+
+        int division = casey / 3;
+            System.out.printf("the division result is:%d%n", + division);
+
+        }
+    }
