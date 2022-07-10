e6494edd6190935c3d18befc4d159d30b1eebce3
Perryjay30 <pelumijsh@gmail.com>
2022-07-09 23:48:03 -1200

    self Tasks

---
+++ src/SelfTask8/SelfTask8.java
@@ -0,0 +1,10 @@
+package SelfTask8;
+
+public class SelfTask8 {
+
+    public static void main(String[] args) {
+
+        System.out.printf("%s%n%s%n%s%n%s%n%s%n%s%n%s%n", "********", " ********", "********",  " ********",
+                "********",  " ********", "********", " ********");
+    }
+}
