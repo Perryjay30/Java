792e68325d39b218f16ff328c6b7de8c2907ac68
Perryjay30 <pelumijsh@gmail.com>
2022-07-15 07:34:45 -1200

    Self Task13

---
+++ src/PerryTheProgrammer7/PositiveAndNegativeNumbers.java
@@ -0,0 +1,11 @@
+package PerryTheProgrammer7;
+
+import java.util.Scanner;
+
+public class PositiveAndNegativeNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+    }
+}
