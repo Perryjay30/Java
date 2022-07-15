3f06b4b3b7fa9cf6db15ba73cd49e7373bdece9d
Perryjay30 <pelumijsh@gmail.com>
2022-07-14 12:34:35 -1200

    self task

---
+++ src/SelfTask9/ArrayOne.java
@@ -0,0 +1,16 @@
+package SelfTask9;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class ArrayOne {
+
+    public static void main(String[] args) {
+        int[][] t = {{2,3},{7,3,1}};
+
+        for(int counter = 0; counter < t.length; counter++) {
+            System.out.print(counter);
+        }
+
+        System.out.println();
+    }
+}
