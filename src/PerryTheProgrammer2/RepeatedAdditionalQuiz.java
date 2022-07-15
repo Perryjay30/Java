836cfb156ce13ca103bfa5ca2ce293a182075a1a
Perryjay30 <pelumijsh@gmail.com>
2022-07-15 07:37:44 -1200

    Perry the Programmer7

---
+++ src/PerryTheProgrammer2/RepeatedAdditionalQuiz.java
@@ -0,0 +1,22 @@
+package PerryTheProgrammer2;
+
+import java.util.Scanner;
+
+public class RepeatedAdditionalQuiz {
+    public static void main(String[] args) {
+        int number1 = (int)(Math.random() * 10);
+        int number2 = (int)(Math.random() * 10);
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is " + number1 + " + " + number2 + "?");
+        int answer = input.nextInt();
+
+        while(number1 + number2 != answer) {
+            System.out.print("Wrong answer. Try again. What is " + number1 + " + " + number2 + "? ");
+            answer = input.nextInt();
+        }
+        System.out.println("You got it");
+    }
+}
--- src/PerryTheProgrammer7/PositiveAndNegativeNumbers.java
+++ src/PerryTheProgrammer7/PositiveAndNegativeNumbers.java
@@ -7,5 +7,19 @@ public class PositiveAndNegativeNumbers {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter positive or negative integers. input ends if it is 0: ");
+        int number = sc.nextInt();
+        float total = 0;
+        float average = 0;
+        while(number != 0) {
+            total += number;
+            average = total / number;
+
+            System.out.println("Enter positive or negative integers. input ends if it is 0: ");
+            number = sc.nextInt();
+            }
+
+        System.out.println("the total is " + total);
+        System.out.println("\nThe average is " + average);
     }
 }
