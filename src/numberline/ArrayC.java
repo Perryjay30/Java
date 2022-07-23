3fcfca9c45b602da9f9336113e2fea2640f5ccfb
Perryjay30 <pelumijsh@gmail.com>
2022-07-22 13:35:47 -1200

    Basic Array

---
+++ src/numberline/ArrayC.java
@@ -0,0 +1,12 @@
+package numberline;
+public class ArrayC {
+
+    public static void main(String[] args) {
+
+        int [] array = {2, 3 ,5,6,7,1,9,8 };
+        int index = 5;
+        int value = 1;
+        System.out.printf("%d", value);
+    }
+
+}
