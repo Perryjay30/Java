bc6e21293e2832fd3aae3ea0f86a90f5d999b9c4
Perryjay30 <pelumijsh@gmail.com>
2022-07-26 16:58:12 -1200

    ClassTask

---
+++ src/forLoop/Interest.java
@@ -0,0 +1,16 @@
+package forLoop;
+
+public class Interest {
+
+    public static void main(String[] args) {
+
+        double principal = 1000.0;
+        double rate = 0.05;
+        System.out.printf("%s%20s%n", "year", "Amount on deposit");
+        for(int year = 1; year <= 10; year++) {
+            double amount = principal * Math.pow(1.0 + rate, year);
+            System.out.printf("%4d%, 20.2f%n", year, amount);
+        }
+
+    }
+}
