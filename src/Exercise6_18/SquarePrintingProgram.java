e52974f1dd7120da02fe300a37e68a43816d0d19
Perryjay30 <pelumijsh@gmail.com>
2022-09-14 14:10:26 -1200

    Exercise6_18

---
+++ src/Exercise6_18/SquarePrintingProgram.java
@@ -0,0 +1,14 @@
+package Exercise6_18;
+
+public class SquarePrintingProgram {
+    public static void solidSquare() {
+       int asterick = 4;
+       for(int i = 0; i < asterick; i++) {
+           System.out.println("*****");
+        }
+    }
+
+    public static void main(String[] args) {
+        solidSquare();;
+    }
+}
