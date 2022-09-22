57c13fa50fd0a654f0c4cf1afc18277551606369
Perryjay30 <pelumijsh@gmail.com>
2022-09-21 14:06:18 -1200

    PrintStream

---
+++ src/Chapter15/IOstreams/PrintStreamSample.java
@@ -0,0 +1,21 @@
+package Chapter15.IOstreams;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class PrintStreamSample {
+    public static void main(String[] args) {
+        String [] cars = {"Honda","Toyota", "Peugeot","Volvo"};
+        try(FileOutputStream fos = new FileOutputStream("cars.txt");
+            PrintStream printStream = new PrintStream(fos)) {
+           System.setOut(printStream);
+           for(String car : cars) {
+               System.out.println(car);
+           }
+            System.out.println("Unicorns");
+        }catch(IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
