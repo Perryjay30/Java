3c9c64d01697f79e4937362f618d43c82d9b10ea
Perryjay30 <pelumijsh@gmail.com>
2022-10-23 07:32:27 -1200

    Exercise8_4 Main

---
+++ src/Exercise8_4/Main.java
@@ -0,0 +1,8 @@
+package Exercise8_4;
+
+public class Main {
+    public static void main(String[] args) {
+        Cylinder gasCooker = new Cylinder(7.00, 15.00);
+        gasCooker.calculate();
+    }
+}
