8d49c86c5707ca43333c3c66aed58f0615ba9da1
Perryjay30 <pelumijsh@gmail.com>
2022-10-31 13:32:42 -1200

    Exercise9_16. Joining inheritance and composition

---
+++ src/Exercise9_16/CommissionCompensationModel.java
@@ -0,0 +1,53 @@
+package Exercise9_16;
+
+public class CommissionCompensationModel extends CompensationModel {
+    private double grossSales; // gross weekly sales
+    private double commissionRate; // commission percentage
+
+    public  CommissionCompensationModel( double grossSales, double commissionRate) {
+
+
+        if (commissionRate <= 0.0 || commissionRate >= 1.0) // validate
+            throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
+
+        if (grossSales < 0.0) // validate
+            throw new IllegalArgumentException("Gross sales must be >= 0.0");
+        this.grossSales = grossSales;
+        this.commissionRate = commissionRate;
+    }
+
+    public void setGrossSales(double grossSales) {
+        if (grossSales < 0.0) // validate
+            throw new IllegalArgumentException("Gross sales must be >= 0.0");
+
+        this.grossSales = grossSales;
+    }
+
+    public double getGrossSales() {
+        return grossSales;
+    }
+
+    public void setCommissionRate(double commissionRate) {
+        if (commissionRate <= 0.0 || commissionRate >= 1.0) // validate
+            throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
+
+        this.commissionRate = commissionRate;
+    }
+
+    public double getCommissionRate() {
+        return commissionRate;
+    }
+
+    public double earnings() {
+
+        return getCommissionRate() * getGrossSales();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s%n%s: $%.2f%n%s: %.2f",
+                "gross sales: ", getGrossSales(),
+                "commission rate: ", getCommissionRate(),
+                "Total Earnings: ", earnings());
+    }
+}
